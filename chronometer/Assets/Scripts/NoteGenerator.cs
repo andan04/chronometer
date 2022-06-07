19e35aa9a77ce2742cf18e594f5d667763eb3acf
andan04 <andan04@naver.com>
2022-06-08 01:34:15 +0900

    노트 파싱 추가

--- chronometer/Assets/Scripts/NoteGenerator.cs
+++ chronometer/Assets/Scripts/NoteGenerator.cs
@@ -1,30 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class NoteGenerator : MonoBehaviour
 {
+    // 시간 바꾸는 노트 리스트, 위, 오른쪽, 아래, 왼쪽 노트 리스트 변수임
     [SerializeField]
     List<int> timeChangerNote;
+    [SerializeField]
     List<int> upNote;
+    [SerializeField]
     List<int> rightNote;
+    [SerializeField]
     List<int> downNote;
+    [SerializeField]
     List<int> leftNote;
+    string[] bmsFile = File.ReadAllLines(@"Assets\BMS\FREEDOMDiVE.bms");
+    [SerializeField]
+    string title;
+    [SerializeField]
+    string artist;
+    [SerializeField]
+    string bpm;
+    [SerializeField]
+    string level;
+
+    int barNum;
+    int noteNum;
+
+
+
 
     private void Awake()
     {
-        
+        StartCoroutine(ReadNote());
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    IEnumerator ReadNote()
+    {
+        print(bmsFile.Length);
+        foreach (string line in bmsFile)
+        {
+            if (line == null)
+            {
+                break;
+            }
+            if (line.StartsWith("#"))
+            {
+                string[] linelist = line.Split(new char[] { ' ', ':' });
+                if (linelist[0] == "#TITLE")
+                {
+                    title = linelist[1];
+                }
+                else if (linelist[0] == "#ARTIST")
+                {
+                    artist = linelist[1];
+                }
+                else if (linelist[0] == "#BPM")
+                {
+                    bpm = linelist[1];
+                }
+                else if (linelist[0] == "#PLAYLEVEL")
+                {
+                    level = linelist[1];
+                }
+                else
+                {
+                    try
+                    {
+                        barNum = int.Parse(linelist[0].Substring(1, 3));
+                        noteNum = int.Parse(linelist[0].Substring(4, 2));
+                        print(barNum + " " + noteNum);
+
+                    }
+                    catch
+                    {
+
+                    }
+                }
+
+            }
+        }
+
+        yield return null;
     }
 }
--- chronometer/Assets/Scripts/TimeChangerScript.cs
+++ chronometer/Assets/Scripts/TimeChangerScript.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class TimeChangerScript : MonoBehaviour
 {
+    // 노트 속도 변수
     [SerializeField]
     float noteSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +17,9 @@ public class TimeChangerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 생성되면 크기 줄어들게 했음
         transform.localScale -= transform.localScale * Time.deltaTime * noteSpeed;
-        if(transform.localScale.x <= 0.5f)
+        if(transform.localScale.x <= 0.5f) // 크기 어느정도 작아지면 없애는거, 나중에 미스 판정도 이쪽에 둬야될듯?
         {
             Destroy(gameObject);
         }
